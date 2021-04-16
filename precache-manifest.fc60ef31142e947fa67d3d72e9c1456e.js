7865b177edab7feda7846b07245d7789f439feb5
unknown <bouaoun.toufik@gmail.com>
2021-04-16 02:05:26 -0400

    Updates

---
+++ precache-manifest.fc60ef31142e947fa67d3d72e9c1456e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f71b94418a24b5a0763",
+    "url": "/myreactweb/static/css/main.2e8963b9.chunk.css"
+  },
+  {
+    "revision": "3f71b94418a24b5a0763",
+    "url": "/myreactweb/static/js/main.3f71b944.chunk.js"
+  },
+  {
+    "revision": "ed107fd7c93934db4d8a",
+    "url": "/myreactweb/static/js/1.ed107fd7.chunk.js"
+  },
+  {
+    "revision": "5a3e48b5e5f727624cf4",
+    "url": "/myreactweb/static/js/runtime~main.5a3e48b5.js"
+  },
+  {
+    "revision": "24a9456e5bde79b9c2ea0addc1f1e48c",
+    "url": "/myreactweb/index.html"
+  }
+];
